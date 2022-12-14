607e5df3d6156a9549313e311c6493d18635fc3a
Karen Grigoryan <karen2000g@mail.ru>
2022-12-14 22:14:07 +0400

    14-12 react

---
+++ react-first/src/Components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import"./style.css";
+
+
+class Header extends Component{
+
+    render(){
+        return <div>
+        <p>helllo world</p>
+        </div>
+    }
+}
+export default Header
